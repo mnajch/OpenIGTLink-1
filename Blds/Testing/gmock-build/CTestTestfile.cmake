ba18f0b3d80324aa8b05c3a11812374c1c56e488
Student <student@ib.polsl.pl>
2017-03-13 13:41:23 +0100

    Zad 2

--- Blds/Testing/gmock-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/mnich2/OpenIGTLink-1/Blds/Testing/gmock
-# Build directory: C:/mnich2/OpenIGTLink-1/Blds/Testing/gmock-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- Blds/Testing/googletest-download/googlemock-prefix/src/googlemock-stamp/verify-googlemock.cmake
+++
@@ -1,4 +0,0 @@
-set(file "C:/mnich2/OpenIGTLink-1/Blds/Testing/googletest-download/googlemock-prefix/src/release-1.7.0.zip")
-message(STATUS "verifying file...
-     file='${file}'")
-message(STATUS "verifying file... warning: did not verify file - no URL_HASH specified?")
