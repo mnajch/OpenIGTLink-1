d99ab13b4f6a97579240067dce62a344354deab0
Student <student@ib.polsl.pl>
2017-03-13 13:22:48 +0100

    dziala odsylanie z serwera

---
+++ Blds/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/mnich2/OpenIGTLink-1
+# Build directory: C:/mnich2/OpenIGTLink-1/Blds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(Source)
+subdirs(Tools)
+subdirs(Examples)
+subdirs(Testing)
---
+++ Blds/Testing/gmock-build/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/mnich2/OpenIGTLink-1/Blds/Testing/gtest
+# Build directory: C:/mnich2/OpenIGTLink-1/Blds/Testing/gmock-build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
